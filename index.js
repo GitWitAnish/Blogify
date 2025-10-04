6c57a2cd30aa8bd8a70521df50e25bbb0bf50871
GitWitAnish <anishkarki5710@gmail.com>
2025-10-04 13:35:32 +0545

    minor changes in index.js

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const path = require('path');
 
+const userRouter = require('./routes/user');
 
 const app = express();
 const port = 8000;
 
+// Middleware
+app.use(express.static(path.resolve('./public')));
 
-
+// View engine setup
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
 
 
+app.get('/', (req, res) => {
+  return res.render('home');
+});
 
 
 
-
+app.use('/user', userRouter);
 
 
 app.listen(port, () => {
