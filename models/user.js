9063698482783afec289d11c66621af40ac8d23c
GitWitAnish <anishkarki5710@gmail.com>
2025-10-04 08:34:01 +0545

    feat: User password hashed

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const { createHmac, randomBytes } = require('node:crypto');
 const {Schema, model} = require('mongoose');
 
 const userSchema = new Schema({
@@ -32,6 +33,21 @@ const userSchema = new Schema({
 }, {
     timestamps: true
 });
+
+userSchema.pre('save', function(next){
+    const user = this;
+    if(!user.isModified('password')) return;
+
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac('sha256',salt)
+          .update(user.password)
+          .digest('hex');
+
+          this.salt = salt;
+          this.password = hashedPassword;
+});
+
+
 
 
 const User = model('User', userSchema);
